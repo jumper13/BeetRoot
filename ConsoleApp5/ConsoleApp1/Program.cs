f52f7612d5a89fd00f23015a03ef218c34636b22
jumper13 <pyatnitca@gmail.com>
2023-05-05 11:22:14 +0200

    HomeWork for Lesson4

--- ConsoleApp5/ConsoleApp1/Program.cs
+++ ConsoleApp5/ConsoleApp1/Program.cs
@@ -2,46 +2,40 @@
 using System;
 using System.Security.Cryptography.X509Certificates;
 
+using System;
+
 class Program
-
-
 {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter two integer values:");
 
-        static void Main(String[] args)
+        int x = 0, y = 0, sum = 0;
+        if (!int.TryParse(Console.ReadLine(), out x) || !int.TryParse(Console.ReadLine(), out y))
+        {
+            Console.WriteLine("Invalid input");
+            return;
+        }
 
-    {     
-          double x = 7.7; // Example Value
-          double y = 5.3; // Example Value
-          var pi = Math.PI;
+        if (X == Y)
+        {
+            sum = X; // if X and Y are equal, sum is one of them
+        }
+        else if (X < Y)
+        {
+            for (int i = X + 1; i < Y; i++) // loop through all integers between X and Y
+            {
+                sum += i; // add each integer to the sum
+            }
+        }
+        else // if X > Y
+        {
+            for (int i = Y + 1; i < X; i++) // loop through all integers between Y and X
+            {
+                sum += i; // add each integer to the sum
+            }
+        }
 
-
-         // -6*x^3+5*x^2-10*x+15
-           double result1 = -6 * Math.Pow(x,3) + 5 * Math.Pow(x, 2) - 10 * x - 15;
-
-        // abs(x)*sin(x)
-          double result2 = Math.Abs(x);
-
-        // 2*pi*x
-          double result3 = 2 * pi * x;
-
-        // max(x, y)
-        double result4 = Math.Max(x, y);
-
-        Console.WriteLine("Result of the first math func:" + result1);    
-        Console.WriteLine("Result of the second math func:" + result2);
-        Console.WriteLine("Result of the third math func:" + result3);
-        Console.WriteLine("Result of the third math func:" + result4);
-
-
-        DateTime now = DateTime.Now;    // Create object DateTime
-        int dayOfYear = now.DayOfYear;  // Get class DayOfYear of object DateTime
-        int restdaysofYear = 366 - dayOfYear;
-
-        Console.WriteLine("Today date is:" + now);
-        Console.WriteLine("Today's day number of Calendar Year is:" + dayOfYear);
-        Console.WriteLine("Untill the end of the year have:" + restdaysofYear);
-
-
-        
+        Console.WriteLine("The sum of all integers between {0} and {1} is {2}.", X, Y, sum);
     }
 }
